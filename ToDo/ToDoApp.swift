e77a4bb4aec1c36dd740ed654c77622ed95427a9
Kristian Mitchell <56667566+kristian821@users.noreply.github.com>
2023-05-24 21:15:50 -0700

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Kristian Mitchell on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
